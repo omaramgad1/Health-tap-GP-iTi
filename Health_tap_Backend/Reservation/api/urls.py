0533f8c202853acfe290715963845c511418b69c
Mohamed Salah <mohamedsalahmasoud173@gmail.com>
2023-06-29 03:51:29 +0300

    creating endPoints for Reservation

--- Health_tap_Backend/Reservation/api/urls.py
+++ Health_tap_Backend/Reservation/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import reservation_list
+from .views import *
 
 urlpatterns = [
-    # path('', reservation_list , name='reservation-list'),
+    path('', list_all_reservation , name='list_all_reservation'),
+    path('doctor/list/reserved/', list_reserved_reservation , name='list-reserved-reservation'),
+    path('doctor/list/cancelled/', list_cancelled_reservation, name='list-cancelled-reservation'),
+    path('doctor/list/Done/', list_done_reservation, name='list-done-reservation'),
     # path('/<int:id>', appointment_detailes , name='appointment-detailes'),
 ]
