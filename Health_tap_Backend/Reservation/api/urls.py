4f4209995382b9457a6fa1c76de7ec94738a393e
Mohamed Salah <mohamedsalahmasoud173@gmail.com>
2023-06-29 15:08:47 +0300

    creating create function in reservation

--- Health_tap_Backend/Reservation/api/urls.py
+++ Health_tap_Backend/Reservation/api/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('patient/list/reserved/', list_reserved_reservation , name='list-reserved-reservation'),
     # path('doctor/list/cancelled/', list_cancelled_reservation, name='list-cancelled-reservation'),
     path('patient/list/Done/', list_done_reservation, name='list-done-reservation'),
-    path('patient/list/<int:appointment_id>/', list_specific_reservation, name='list-specific-reservation'),
+    path('patient/list/<int:reservation_id>/', list_specific_reservation, name='list-specific-reservation'),
     
     # path('/<int:id>', appointment_detailes , name='appointment-detailes'),
 ]
--- Health_tap_Backend/Reservation/api/views.py
+++ Health_tap_Backend/Reservation/api/views.py
@@ -1,10 +1,12 @@
 from rest_framework.decorators  import  api_view, permission_classes
 from rest_framework.response import Response
 from Reservation.models import Reservation
+from Appointment.models import Appointment
 from User.models import Patient
 from .serializers import ReservationSerializer
 from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import get_object_or_404
+from rest_framework import status
 
 
 
@@ -44,9 +46,23 @@ def list_done_reservation(request):
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
-def list_specific_reservation(request, appointment_id):
+def list_specific_reservation(request, reservation_id):
     patient = get_object_or_404(Patient , user = request.user)
-    reservation = get_object_or_404(Reservation, appointment_id=appointment_id, patient=patient)
+    reservation = get_object_or_404(Reservation, reservation_id=reservation_id, patient=patient)
     serializer = ReservationSerializer(reservation)
     return Response(serializer.data)
 
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def create_reservation(request, appointment_id):
+    patient = get_object_or_404(Patient, user=request.user)
+    appointment = get_object_or_404(Appointment, id=appointment_id)
+    if appointment.status == 'A':
+        reservation = Reservation.objects.create(patient=patient, appointment=appointment, status='R')
+        appointment.status = 'R'
+        appointment.save()
+        serializer = ReservationSerializer(reservation)
+        return Response(serializer.data)
+
+    return Response({'message': 'Appointment is not available.'}, status=status.HTTP_400_BAD_REQUEST)
