81673a30f67926b7a81bb30baa8ecd95ab18c084
Mohamed Salah <mohamedsalahmasoud173@gmail.com>
2023-06-29 04:55:22 +0300

    modify get functions and adding to get specific reservation for patient

--- Health_tap_Backend/Reservation/api/urls.py
+++ Health_tap_Backend/Reservation/api/urls.py
@@ -3,8 +3,10 @@ from .views import *
 
 urlpatterns = [
     path('', list_all_reservation , name='list_all_reservation'),
-    path('doctor/list/reserved/', list_reserved_reservation , name='list-reserved-reservation'),
-    path('doctor/list/cancelled/', list_cancelled_reservation, name='list-cancelled-reservation'),
-    path('doctor/list/Done/', list_done_reservation, name='list-done-reservation'),
+    path('patient/list/reserved/', list_reserved_reservation , name='list-reserved-reservation'),
+    # path('doctor/list/cancelled/', list_cancelled_reservation, name='list-cancelled-reservation'),
+    path('patient/list/Done/', list_done_reservation, name='list-done-reservation'),
+    path('patient/list/<int:appointment_id>/', list_specific_reservation, name='list-specific-reservation'),
+    
     # path('/<int:id>', appointment_detailes , name='appointment-detailes'),
 ]
--- Health_tap_Backend/Reservation/api/views.py
+++ Health_tap_Backend/Reservation/api/views.py
@@ -1,36 +1,52 @@
 from rest_framework.decorators  import  api_view, permission_classes
 from rest_framework.response import Response
 from Reservation.models import Reservation
+from User.models import Patient
 from .serializers import ReservationSerializer
 from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
 
 
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def list_all_reservation(request):
-    reservations = Reservation.objects.all()
+    print ('********************************')
+    print (request.user)
+    print ('********************************')
+    patient = get_object_or_404(Patient , user = request.user)
+    reservations = Reservation.objects.filter(patient=patient)
     serializer = ReservationSerializer(reservations, many=True)
     return Response(serializer.data)
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def list_reserved_reservation(request):
-    reservations = Reservation.objects.filter(status='R')
+    patient = get_object_or_404(Patient , user = request.user)
+    reservations = Reservation.objects.filter( patient=patient, status='R')
     serializer = ReservationSerializer(reservations, many=True)
     return Response(serializer.data)
 
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def list_cancelled_reservation(request):
-    reservations = Reservation.objects.filter(status='C')
-    serializer = ReservationSerializer(reservations, many=True)
-    return Response(serializer.data)
+# @api_view(['GET'])
+# @permission_classes([IsAuthenticated])
+# def list_cancelled_reservation(request):
+#     reservations = Reservation.objects.filter(status='C')
+#     serializer = ReservationSerializer(reservations, many=True)
+#     return Response(serializer.data)
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def list_done_reservation(request):
-    reservations = Reservation.objects.filter(status='D')
+    patient = get_object_or_404(Patient , user = request.user)
+    reservations = Reservation.objects.filter( patient=patient, status='D')
     serializer = ReservationSerializer(reservations, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def list_specific_reservation(request, appointment_id):
+    patient = get_object_or_404(Patient , user = request.user)
+    reservation = get_object_or_404(Reservation, appointment_id=appointment_id, patient=patient)
+    serializer = ReservationSerializer(reservation)
     return Response(serializer.data)
 
