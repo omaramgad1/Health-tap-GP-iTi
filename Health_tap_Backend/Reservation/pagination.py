137b304edc295ddcc883d12ad3d88819f7ef5775
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2023-07-02 07:16:21 +0300

    pagination in reserved list

---
+++ Health_tap_Backend/Reservation/pagination.py
@@ -0,0 +1,6 @@
+from rest_framework.pagination import PageNumberPagination
+
+class ReservedListPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = 'size'
+    max_page_size = 10
