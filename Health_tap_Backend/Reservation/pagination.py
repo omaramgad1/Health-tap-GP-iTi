35e0d4a70a67ba6917893ced525137a6d025ea27
Mohamed Salah <mohamedsalahmasoud173@gmail.com>
2023-07-04 06:51:15 +0300

    modify pagination and model

--- Health_tap_Backend/Reservation/pagination.py
+++ Health_tap_Backend/Reservation/pagination.py
@@ -3,4 +3,4 @@ from rest_framework.pagination import PageNumberPagination
 class ReservedListPagination(PageNumberPagination):
     page_size = 5
     page_size_query_param = 'size'
-    max_page_size = 10
+    max_page_size = 20
